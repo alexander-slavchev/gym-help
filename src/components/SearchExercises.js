451e9f1ec99329a088825b5e2863b7e7334bbd56
aleksandar <a.slavchev19.48@gmail.com>
2024-11-30 01:48:32 +0200

    fetching data

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
+
 const SearchExercises = () => {
     const [search, setSearch] = useState('')
+    const [exercises, setExercises] = useState([])
+    const [bodyParts, setBodyParts] = useState([])
+
+    useEffect(() =>{
+      const fetchExercisesData = async () => {
+        const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      
+        setBodyParts(['all', ...bodyPartsData]);
+      }
+
+      fetchExercisesData();
+    }, [])
   
     const handleSearch = async () => {
       if (search) {
+        const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
         
+        const searchedExercises = exerciseData.filter(
+          (exercise) => exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+        );
+
+        setSearch('');
+        setExercises(searchedExercises);
       }
     };
   
@@ -28,6 +53,9 @@ const SearchExercises = () => {
           <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
             Search
           </Button>
+        </Box>
+        <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
+            <HorizontalScrollbar data={bodyParts}/>
         </Box>
         
       </Stack>
