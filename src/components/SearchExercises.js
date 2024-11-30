7e281ae847828b4ad5fff5de9e9009d806bb8169
aleksandar <a.slavchev19.48@gmail.com>
2024-11-30 02:00:57 +0200

    horizontal scrollbar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -55,7 +55,8 @@ const SearchExercises = () => {
           </Button>
         </Box>
         <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
-            <HorizontalScrollbar data={bodyParts}/>
+            <HorizontalScrollbar data={bodyParts}
+            bodyPart={bodyPart} setBodyPart={setBodyPart}/>
         </Box>
         
       </Stack>
