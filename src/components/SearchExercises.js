791d06b9a6dc968e506c8e8d24a5af0b3d7ad7f9
aleksandar <a.slavchev19.48@gmail.com>
2024-11-30 02:02:15 +0200

    horizontal scrollbar2

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,9 +4,9 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
     const [search, setSearch] = useState('')
-    const [exercises, setExercises] = useState([])
+    
     const [bodyParts, setBodyParts] = useState([])
 
     useEffect(() =>{
